f5a27b9bb15c444d3cdf3299098a87fb697c53e9
valcor1 <cex@Sedars-MacBook.local>
2016-04-26 12:09:08 -0400

    Battleship algorithm_day_1
    Basic algorithm writing starting with peosudo and now placing parts of
    code block or example code in its due peosudo code portions

---
+++ main.cpp
@@ -0,0 +1,60 @@
+//
+//  main.cpp
+//  CSIS_111_lab_9
+//
+//  Created by Sedar  on 4/25/16.
+//  Copyright (c) 2016 David D'Haiti. All rights reserved.
+//
+
+//                      DO NOT CODE TILL ALGORITHIM IS CREATED
+
+
+ /*
+#include <iostream>
+
+
+using namespace std;
+
+int main() {
+ 
+    for (<#initialization#>; <#condition#>; <#increment#>) {
+        <#statements#>
+    }
+    return 0;
+}
+*/
+//                      DO NOT CODE TILL ALGORITHIM IS CREATED
+
+/*
+ 1.CREATE 2 TABLES 
+    - ONE TABLE IN A FILE (done)
+         ~~~~####~~
+         ~~#~~~~~~~
+         ~~#~~~~~~~
+         ~~#~~~~~~~
+         ~~#~~~#~~~
+         ~~~~~~~#~~
+         ~~~~~~~~#~
+         ~~~~###~~~
+         ~~~~~~~~~~
+         ~~~~~~~~~~
+    - ONE TABLE FOR THE USER THEMSELVES
+        *look back at bar chart lab 
+        * Can a strut be used outside of the function in which it is held
+ 
+ 2. SHOW BOARDS 
+    * THIS IS HOW TO SHOW THE UNMARKED CHART TO THE USER
+        - for (int i = 0 ; i < xAxis; i++){
+            cout << '&';
+                for(int j = 0; j < width;j++;{
+                    cout << setw(5) << '&';}
+        }
+ 3. PROMPT PLAYER ON HOW TO ATTACK A CERTIAN AREA
+ 4. PROMPT PLAYER TO ATTACK
+ 5. ATTACK ACTION
+ 6. ATTACK RESPONSE 
+ 7. GAME WIN / LOSE RESPONSE 
+ 8. TELL THE PLAYER THE NUMBER OF TIMES THAT ATTACKED
+ 
+ 
+ */
